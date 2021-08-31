8718b29b96a0addaf3e754f839a10750744a7cb4
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-31 17:14:49 +0200

    app component with routes and layout component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Quotes from './pages/Quotes';
+import QuoteShow from './pages/QuoteShow';
+import AddQuote from './pages/AddQuote';
+import NotFound from './pages/NotFound';
+
+import Layout from './components/layout/Layout';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/quotes' />
+        </Route>
+        <Route path='/quotes' exact>
+          <Quotes />
+        </Route>
+        <Route path='/quotes/:quoteId'>
+          <QuoteShow />
+        </Route>
+        <Route path='/new-quote'>
+          <AddQuote />
+        </Route>
+        <Route path='*'>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
