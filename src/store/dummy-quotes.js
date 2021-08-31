0091fb9ff6f2ddd88b978362e4399bd14360aae7
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-31 17:19:32 +0200

    dummy data

---
+++ src/store/dummy-quotes.js
@@ -0,0 +1,13 @@
+export const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Bob', text: "Don't worry be happy" },
+  {
+    id: 'q2',
+    author: 'Marilyn Monroe',
+    text: 'Beauty is in the eye of the beerholder',
+  },
+  {
+    id: 'q3',
+    author: 'Honest Abe Lincoln',
+    text: 'Most quotes on the internet are made up',
+  },
+];
