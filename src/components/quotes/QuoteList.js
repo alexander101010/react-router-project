14011750349e063cddacda51664bef6bf200d02b
Alexander Botwinick <alexbotwinick@gmail.com>
2021-09-01 11:27:04 +0200

    add sort logic

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,43 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
+import { sortQuotes } from '../../js/helpers';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const isSortingAscending = queryParams.get('sort') === 'asc'; // get is built into URLSearchParams and 'sort' is the key in the query string
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    /*
+    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+    history.push(
+      `${location.pathname}?sort=${isSortingAscending ? 'desc' : 'asc'}`
+    );
+    */
+
+    // You can give Link or History an object instead of string to improve readability if your url is complex and dynamic
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${isSortingAscending ? 'desc' : 'asc'}`,
+    });
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
