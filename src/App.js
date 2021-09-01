156aabbc0cdd19e6fbc316dcacb3b23f371e1dbd
Alexander Botwinick <alexbotwinick@gmail.com>
2021-09-01 16:14:56 +0200

    lazy loading refactor of AddQuote comp

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
+import React, { Suspense } from 'react';
+
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Quotes from './pages/Quotes';
 import QuoteShow from './pages/QuoteShow';
-import AddQuote from './pages/AddQuote';
+// import AddQuote from './pages/AddQuote';
 import NotFound from './pages/NotFound';
 
 import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const AddQuote = React.lazy(() => import('./pages/AddQuote'));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
-        </Route>
-        <Route path='/quotes' exact>
-          <Quotes />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteShow />
-        </Route>
-        <Route path='/new-quote'>
-          <AddQuote />
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className='centered'>
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes' />
+          </Route>
+          <Route path='/quotes' exact>
+            <Quotes />
+          </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteShow />
+          </Route>
+          <Route path='/new-quote'>
+            <AddQuote />
+          </Route>
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
