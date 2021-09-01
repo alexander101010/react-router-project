e35c7d90d16615fe24144a79abd181e6e8272b27
Alexander Botwinick <alexbotwinick@gmail.com>
2021-09-01 13:56:57 +0200

    get quote from db not dummy data

--- src/pages/QuoteShow.js
+++ src/pages/QuoteShow.js
@@ -1,25 +1,57 @@
+import { useEffect } from 'react';
 import { Route, useParams, Link, useRouteMatch } from 'react-router-dom';
 
-import { DUMMY_QUOTES } from '../store/dummy-quotes';
+import { getSingleQuote } from '../lib/api';
+import useHttp from '../hooks/use-http';
 
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 
 const QuoteShow = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
   const match = useRouteMatch();
   const params = useParams();
+  const { quoteId } = params;
 
-  const currentQuote = DUMMY_QUOTES.find(
-    (quote) => quote.id === params.quoteId
-  );
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
-  if (!currentQuote) {
-    return <p>Sorry, no quote found.</p>;
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className='centered focused'>
+        <p>{error}</p>
+      </div>
+    );
+  }
+
+  if (status === 'completed' && !loadedQuote.text) {
+    return (
+      <div className='centered'>
+        <p>No quote found </p>
+      </div>
+    );
   }
 
   return (
     <section>
-      <HighlightedQuote author={currentQuote.author} text={currentQuote.text} />
+      <HighlightedQuote author={loadedQuote.author} text={loadedQuote.text} />
       {/* <Route path={`/quotes/${params.quoteId}/`} exact> */}
       <Route path={match.path} exact>
         <div className='centered'>
