de0972a7e86f253dc10e5e53fad7d110d90b6f38
Alexander Botwinick <alexbotwinick@gmail.com>
2021-09-01 11:28:35 +0200

    refactor routes to make them dynamic with useRouteMatch

--- src/pages/QuoteShow.js
+++ src/pages/QuoteShow.js
@@ -1,4 +1,4 @@
-import { Route, useParams } from 'react-router-dom';
+import { Route, useParams, Link, useRouteMatch } from 'react-router-dom';
 
 import { DUMMY_QUOTES } from '../store/dummy-quotes';
 
@@ -6,6 +6,7 @@ import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
 const QuoteShow = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
   const currentQuote = DUMMY_QUOTES.find(
@@ -19,7 +20,15 @@ const QuoteShow = () => {
   return (
     <section>
       <HighlightedQuote author={currentQuote.author} text={currentQuote.text} />
-      <Route path='/quotes/:quoteId/comments'>
+      {/* <Route path={`/quotes/${params.quoteId}/`} exact> */}
+      <Route path={match.path} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`${match.url}/comments`}>
+            View Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </section>
