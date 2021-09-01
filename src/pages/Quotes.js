8f9668032a5c0d7614b52939b98876f61b715b79
Alexander Botwinick <alexbotwinick@gmail.com>
2021-09-01 13:56:40 +0200

    get quotes from DB instead of dummy data

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,12 +1,48 @@
-import QuoteList from '../components/quotes/QuoteList';
+import { useEffect } from 'react';
 
-import { DUMMY_QUOTES as quotes } from '../store/dummy-quotes';
+import QuoteList from '../components/quotes/QuoteList';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
 
 const Quotes = (props) => {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div className='centered focused'>{error}</div>;
+  }
+
+  if ((status === 'completed' && !loadedQuotes) || loadedQuotes.length === 0) {
+    return (
+      <div className='centered'>
+        <NoQuotesFound />
+      </div>
+    );
+  }
+
   return (
     <section>
       <h1>All Quotes 💬</h1>
-      <QuoteList quotes={quotes} />
+      <QuoteList quotes={loadedQuotes} />
     </section>
   );
 };
