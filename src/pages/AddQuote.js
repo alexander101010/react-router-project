8bedd81d2d09b1aff634179f18332f07d42bd4f7
Alexander Botwinick <alexbotwinick@gmail.com>
2021-09-01 13:56:21 +0200

    add logic for http post of new quote

--- src/pages/AddQuote.js
+++ src/pages/AddQuote.js
@@ -1,20 +1,34 @@
+import { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
 
 const AddQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
-  const addQuoteToArrayHandler = (quoteObj) => {
-    console.log(quoteObj);
+
+  useEffect(() => {
+    if (status === 'completed') {
+      history.push('/quotes');
+    }
+  }, [status, history]);
+
+  const addQuoteHandler = (quoteObj) => {
+    sendRequest(quoteObj);
 
     // Navigate programitically to All Quotes ****
     // history.push - we can go back with history
-    // history.replace - adds new page
-    history.push('/quotes');
+    // history.replace - adds new page, can't use back button
+    // history.push('/quotes');
   };
 
   return (
     <section>
-      <QuoteForm onAddQuote={addQuoteToArrayHandler} isLoading={false} />
+      <QuoteForm
+        onAddQuote={addQuoteHandler}
+        isLoading={status === 'pending'}
+      />
     </section>
   );
 };
